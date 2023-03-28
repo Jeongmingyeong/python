9148931f5f99c8f370c3dd7059025faabe9dbb75
Jeongmingyeong <jmink002@gmail.com>
2023-03-28 12:47:06 +0900

    fix too many positional arguments

--- too many positional arguments.py
+++ too many positional arguments.py
@@ -1,19 +1,10 @@
-import sys # 3. never used
+def sum(a, b):
+    return a + b
 
-class Person:
-    def __init__(self, first_name,  last_name, age):
-        self.first_name = first_name
-        self.last_name = last_name
-        self.age = age
+# many positional argument
+result = sum(1, 2, 3)
+print(result)
 
-    if "Windows" in platform.platform():
-        print("You're using Windows!")
-
-    self.age = self.getAge(1,2,3)
-
-    def getAge(this):
-        return "18"
-
-# it has 4 issues
-# 1. "too many positional arguments" error in the call of the method getAge
-# 2. The getAge method it
+# missing argument
+result = sum(1)
+print(result)
